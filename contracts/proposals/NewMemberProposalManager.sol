6d464f79c3c1f3b91f2940ecfcc2e8079d498592
jonybang <Jonybange@gmail.com>
2019-02-09 01:40:56 +0200

    add arrays to new proposal manager

--- contracts/proposals/NewMemberProposalManager.sol
+++ contracts/proposals/NewMemberProposalManager.sol
@@ -16,14 +16,19 @@ pragma solidity 0.5.3;
 import "../FundStorage.sol";
 import "./AbstractProposalManager.sol";
 
-
 contract NewMemberProposalManager is AbstractProposalManager {
+  using ArraySet for ArraySet.Uint256Set;
+  
   struct Proposal {
     uint256 spaceTokenId;
     string description;
+    address sender;
   }
 
   mapping(uint256 => Proposal) private _proposals;
+  
+  mapping(address => ArraySet.Uint256Set) private _activeProposalsBySender;
+  ArraySet.Uint256Set private _activeProposals;
 
   constructor(IRSRA _rsra, FundStorage _fundStorage) public AbstractProposalManager(_rsra, _fundStorage) {
   }
@@ -33,7 +38,8 @@ contract NewMemberProposalManager is AbstractProposalManager {
 
     _proposals[id] = Proposal({
       spaceTokenId: _spaceTokenId,
-      description: _description
+      description: _description,
+      sender: msg.sender
     });
 
     emit NewProposal(id, msg.sender);
@@ -41,12 +47,18 @@ contract NewMemberProposalManager is AbstractProposalManager {
     ProposalVoting storage proposalVoting = _proposalVotings[id];
 
     proposalVoting.status = ProposalStatus.ACTIVE;
+
+    _activeProposalsBySender[msg.sender].add(id);
+    _activeProposals.add(id);
   }
 
   function _execute(uint256 _proposalId) internal {
     Proposal storage p = _proposals[_proposalId];
 
     fundStorage.approveMint(p.spaceTokenId);
+
+    _activeProposalsBySender[p.sender].remove(_proposalId);
+    _activeProposals.remove(_proposalId);
   }
 
   function getThreshold() public view returns (uint256) {
@@ -57,5 +69,21 @@ contract NewMemberProposalManager is AbstractProposalManager {
     Proposal storage p = _proposals[_proposalId];
 
     return (p.spaceTokenId, p.description);
+  }
+  
+  function getActiveProposals() external view returns (uint256[] memory) {
+    return _activeProposals.elements();
+  }
+
+  function getActiveProposalsCount() external view returns (uint256) {
+    return _activeProposals.size();
+  }
+
+  function getActiveProposalsBySender(address sender) external view returns (uint256[] memory) {
+    return _activeProposalsBySender[sender].elements();
+  }
+
+  function getActiveProposalsBySenderCount(address sender) external view returns (uint256) {
+    return _activeProposalsBySender[sender].size();
   }
 }
