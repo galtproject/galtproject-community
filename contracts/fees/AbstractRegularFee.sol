3ce2deeec27d4efdad596212c70b17998f16cfd8
Nikita Chebykin <nikita@chebyk.in>
2019-03-06 16:54:58 +0200

    Add RegularFeeFactory; Closes #49

---
+++ contracts/fees/AbstractRegularFee.sol
@@ -0,0 +1,30 @@
+/*
+ * Copyright ©️ 2018 Galt•Space Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka),
+ * [Dima Starodubcev](https://github.com/xhipster),
+ * [Valery Litvin](https://github.com/litvintech) by
+ * [Basic Agreement](http://cyb.ai/QmSAWEG5u5aSsUyMNYuX2A2Eaz4kEuoYWUkVBRdmu9qmct:ipfs)).
+ *
+ * Copyright ©️ 2018 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) and
+ * Galt•Space Society Construction and Terraforming Company by
+ * [Basic Agreement](http://cyb.ai/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS:ipfs)).
+ */
+
+pragma solidity 0.5.3;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../FundStorage.sol";
+import "./interfaces/IRegularFee.sol";
+
+
+// TODO: extract payment specific functions in order to make this contract abstract from a payment method
+contract AbstractRegularFee is IRegularFee {
+  // currency
+  FundStorage fundStorage;
+
+  constructor (FundStorage _fundStorage) public {
+    fundStorage = _fundStorage;
+  }
+}
---
+++ contracts/fees/RegularErc20Fee.sol
@@ -0,0 +1,27 @@
+/*
+ * Copyright ©️ 2018 Galt•Space Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka),
+ * [Dima Starodubcev](https://github.com/xhipster),
+ * [Valery Litvin](https://github.com/litvintech) by
+ * [Basic Agreement](http://cyb.ai/QmSAWEG5u5aSsUyMNYuX2A2Eaz4kEuoYWUkVBRdmu9qmct:ipfs)).
+ *
+ * Copyright ©️ 2018 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) and
+ * Galt•Space Society Construction and Terraforming Company by
+ * [Basic Agreement](http://cyb.ai/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS:ipfs)).
+ */
+
+pragma solidity 0.5.3;
+
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+import "../FundStorage.sol";
+import "./AbstractRegularFee.sol";
+
+
+contract RegularErc20Fee  is AbstractRegularFee {
+  IERC20 token;
+
+  constructor (IERC20 _token, FundStorage _fundStorage) public AbstractRegularFee(_fundStorage) {
+    token = _token;
+  }
+}
