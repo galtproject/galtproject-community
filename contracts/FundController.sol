449eea4cc3e46e797154440af0755b5b29a10250
Nikita Chebykin <nikita@chebyk.in>
2019-05-21 19:52:19 +0200

    Remove permissionable

--- contracts/FundController.sol
+++ contracts/FundController.sol
@@ -15,13 +15,12 @@ pragma solidity 0.5.7;
 
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "@galtproject/libs/contracts/traits/Permissionable.sol";
 import "@galtproject/libs/contracts/collections/ArraySet.sol";
 import "./FundMultiSig.sol";
 import "./FundStorage.sol";
 
 
-contract FundController is Permissionable {
+contract FundController {
   using ArraySet for ArraySet.AddressSet;
 
   enum Currency {
