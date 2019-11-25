ff113b4d4cfcfa564b0c37c8d226131e90026ec1
jonybang <Jonybange@gmail.com>
2019-11-25 17:12:53 +0200

    add MockFundStorage

---
+++ contracts/mocks/MockFundStorageFactory.sol
@@ -0,0 +1,45 @@
+/*
+ * Copyright ©️ 2018 Galt•Project Society Construction and Terraforming Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka)
+ *
+ * Copyright ©️ 2018 Galt•Core Blockchain Company
+ * (Founded by [Nikolai Popeka](https://github.com/npopeka) by
+ * [Basic Agreement](ipfs/QmaCiXUmSrP16Gz8Jdzq6AJESY1EAANmmwha15uR3c1bsS)).
+ */
+
+pragma solidity 0.5.10;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "@galtproject/core/contracts/registries/GaltGlobalRegistry.sol";
+
+// This contract will be included into the current one
+import "./MockFundStorage.sol";
+
+
+contract MockFundStorageFactory is Ownable {
+  function build(
+    GaltGlobalRegistry _ggr,
+    bool _isPrivate,
+    uint256 _defaultProposalSupport,
+    uint256 _defaultProposalQuorum,
+    uint256 _defaultProposalTimeout,
+    uint256 _periodLength
+  )
+    external
+    returns (MockFundStorage)
+  {
+    MockFundStorage fundStorage = new MockFundStorage(
+      _ggr,
+      _isPrivate,
+      _defaultProposalSupport,
+      _defaultProposalQuorum,
+      _defaultProposalTimeout,
+      _periodLength
+    );
+
+    fundStorage.addRoleTo(msg.sender, "role_manager");
+    fundStorage.removeRoleFrom(address(this), "role_manager");
+
+    return fundStorage;
+  }
+}
